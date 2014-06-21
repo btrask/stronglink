05a626d22303d274559e094073df9ab8a2c0b178
Ben Trask <bentrask@comcast.net>
2014-06-20 20:46:13 -0400

    Mix and match your own crash!
    Factors:
    - Stack size (smaller = more likely to crash)
    - co_terminate vs yield (term = more likely)
    - Work done on thread (more work = more likely)
    - VFS selected (ours = more likely)
    - Number of threads (more threads = more likely)
    
    Presumaby this is another use-after-free type bug.
    Something in the thread is still important when it gets overwritten?
    
    I feel like the malloc() implementation on OS X has a lot of debug flags that would've helped us catch this by now.

--- test/test_sqlite_async.c
+++ test/test_sqlite_async.c
@@ -9,10 +9,10 @@ uv_loop_t *loop = NULL;
 void sqlite_async_register(void);
 
 void test_thread(void) {
-//	int err = 0;
+	int err = 0;
 
 //for(index_t i = 0; i < 20; ++i) {
-/*	sqlite3 *db = NULL;
+	sqlite3 *db = NULL;
 	err = sqlite3_open_v2(
 		NULL, // TODO: Test real paths too.
 		&db,
@@ -20,9 +20,9 @@ void test_thread(void) {
 		NULL
 	);
 	assert(SQLITE_OK == err);
-	assert(NULL != db);*/
+	assert(NULL != db);
 
-/*	EXEC(QUERY(db,
+	EXEC(QUERY(db,
 		"CREATE TABLE IF NOT EXISTS test (\n"
 		"\t" "a INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n"
 		"\t" "b TEXT,\n"
@@ -35,13 +35,13 @@ void test_thread(void) {
 		EXEC(QUERY(db, "SELECT * FROM test"));
 		EXEC(QUERY(db, "INSERT INTO test (b, c) VALUES ('asdf', 'jkl;')"));
 		EXEC(QUERY(db, "COMMIT"));
-	}*/
+	}
 
-//	err = sqlite3_close(db);
-//	assert(SQLITE_OK == err);
+	err = sqlite3_close(db);
+	assert(SQLITE_OK == err);
 //}
-	co_terminate();
-//	co_switch(yield);
+//	co_terminate();
+	co_switch(yield);
 }
 
 /*static sqlite3_mutex *m1;
@@ -60,15 +60,15 @@ void test_mutex(void) {
 	co_switch(yield);
 }*/
 
-#define STACK_SIZE (1024 * 1000 * sizeof(void *) / 4)
+#define STACK_SIZE (1024 * 50 * sizeof(void *) / 4)
 int main() {
 	yield = co_active();
 	loop = uv_default_loop();
 //	sqlite_async_register();
 
-//	for(index_t i = 0; i < 20; ++i) {
+	for(index_t i = 0; i < 20; ++i) {
 		co_switch(co_create(STACK_SIZE, test_thread));
-//	}
+	}
 	uv_run(loop, UV_RUN_DEFAULT);
 
 /*	m1 = sqlite3_mutex_alloc(SQLITE_MUTEX_RECURSIVE);
