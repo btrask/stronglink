6e95227590f1ec020834d651ba44cb0cfd3703a8
Ben Trask <bentrask@comcast.net>
2015-04-26 21:27:45 -0400

    Make bcrypt wrapper a little less embarrassing.

--- src/util/bcrypt.c
+++ src/util/bcrypt.c
@@ -4,6 +4,10 @@
 #include "../async/async.h"
 #include "bcrypt.h"
 
+#define BCRYPT_PREFIX "$2a$" /* TODO: Are we supposed to use "$2y$" now? */
+#define BCRYPT_ROUNDS 13 /* TODO: Possibly reduce this to 12. */
+#define BCRYPT_SALT 16
+
 bool checkpass(char const *const pass, char const *const hash) {
 	int size = 0;
 	void *data = NULL;
@@ -16,13 +20,14 @@ bool checkpass(char const *const pass, char const *const hash) {
 char *hashpass(char const *const pass) {
 	// TODO: async_random isn't currently parallel or thread-safe
 //	async_pool_enter(NULL);
-	char input[GENSALT_INPUT_SIZE];
-	if(async_random((unsigned char *)input, GENSALT_INPUT_SIZE) < 0) {
+	char input[BCRYPT_SALT];
+	if(async_random((unsigned char *)input, BCRYPT_SALT) < 0) {
 //		async_pool_leave(NULL);
 		return NULL;
 	}
 	async_pool_enter(NULL); // TODO (above)
-	char *salt = crypt_gensalt_ra("$2a$", 13, input, GENSALT_INPUT_SIZE); // TODO: Use `$2y$` now? bcrypt library needs updating.
+
+	char *salt = crypt_gensalt_ra(BCRYPT_PREFIX, BCRYPT_ROUNDS, input, BCRYPT_SALT);
 	if(!salt) {
 		async_pool_leave(NULL);
 		return NULL;
--- src/util/bcrypt.h
+++ src/util/bcrypt.h
@@ -1,7 +1,5 @@
 #include <stdbool.h>
 
-#define GENSALT_INPUT_SIZE 16
-
 bool checkpass(char const *const pass, char const *const hash);
 char *hashpass(char const *const pass);
 
