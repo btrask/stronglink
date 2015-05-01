551c0d7512186d54108dd6a2db1be6068999cf89
Ben Trask <bentrask@comcast.net>
2015-05-01 01:25:23 -0400

    SLNCollectionFilter minor cleanup.

--- src/filter/SLNCollectionFilter.m
+++ src/filter/SLNCollectionFilter.m
@@ -48,7 +48,7 @@ static int filtercmp_rev(SLNFilter *const *const a, SLNFilter *const *const b) {
 - (int)prepare:(DB_txn *const)txn {
 	int rc = [super prepare:txn];
 	if(DB_SUCCESS != rc) return rc;
-	for(size_t i = 0; i < count; ++i) {
+	for(size_t i = 0; i < count; i++) {
 		rc = [filters[i] prepare:txn];
 		if(DB_SUCCESS != rc) return rc;
 	}
@@ -57,20 +57,14 @@ static int filtercmp_rev(SLNFilter *const *const a, SLNFilter *const *const b) {
 }
 - (void)seek:(int const)dir :(uint64_t const)sortID :(uint64_t const)fileID {
 	assert(count);
-	for(size_t i = 0; i < count; ++i) {
+	for(size_t i = 0; i < count; i++) {
 		[filters[i] seek:dir :sortID :fileID];
 	}
 	[self sort:dir ? dir : +1];
 }
 - (void)current:(int const)dir :(uint64_t *const)sortID :(uint64_t *const)fileID {
 	assert(count);
-	// TODO: The current value shouldn't actually depend on which direction
-	// the client wants to go. We shouldn't even accept it as an argument.
-	// That would mean getting rid of invalid() and instead returning a
-	// single invalid error regardless of intended direction, like
-	// DB_NOTFOUND. Whether that's a good idea or not is yet to be seen.
-	if(0 == sort) {
-		assert(0); // TODO
+	if(0 == sort) { // Means we don't have a valid position.
 		if(sortID) *sortID = invalid(dir);
 		if(fileID) *fileID = invalid(dir);
 	}
@@ -82,12 +76,12 @@ static int filtercmp_rev(SLNFilter *const *const a, SLNFilter *const *const b) {
 	assert(0 != sort); // Means we don't have a valid position.
 	uint64_t oldSortID, oldFileID;
 	[filters[0] current:dir :&oldSortID :&oldFileID];
-	if(dir != sort) {
+	if((dir > 0) != (sort > 0)) {
 		// Flip directions. Inexact sub-filters must be repositioned.
 		[self seek:dir :oldSortID :oldFileID];
 	}
 	[filters[0] step:dir];
-	for(size_t i = 1; i < count; ++i) {
+	for(size_t i = 1; i < count; i++) {
 		uint64_t curSortID, curFileID;
 		[filters[i] current:dir :&curSortID :&curFileID];
 		if(curSortID != oldSortID || curFileID != oldFileID) break;
@@ -110,18 +104,18 @@ static int filtercmp_rev(SLNFilter *const *const a, SLNFilter *const *const b) {
 - (SLNFilterType)type {
 	return SLNIntersectionFilterType;
 }
-- (void)print:(count_t const)depth {
+- (void)print:(size_t const)depth {
 	indent(depth);
 	fprintf(stderr, "(intersection\n");
-	for(size_t i = 0; i < count; ++i) [filters[i] print:depth+1];
+	for(size_t i = 0; i < count; i++) [filters[i] print:depth+1];
 	indent(depth);
 	fprintf(stderr, ")\n");
 }
-- (size_t)getUserFilter:(str_t *const)data :(size_t const)size :(count_t const)depth {
+- (size_t)getUserFilter:(str_t *const)data :(size_t const)size :(size_t const)depth {
 	if(!count) return wr(data, size, "");
 	size_t len = 0;
 	if(depth) len += wr(data+len, size-len, "(");
-	for(size_t i = 0; i < count; ++i) {
+	for(size_t i = 0; i < count; i++) {
 		if(i) len += wr(data+len, size-len, " ");
 		len += [filters[i] getUserFilter:data+len :size-len :depth+1];
 	}
@@ -156,16 +150,16 @@ static int filtercmp_rev(SLNFilter *const *const a, SLNFilter *const *const b) {
 - (SLNFilterType)type {
 	return SLNUnionFilterType;
 }
-- (void)print:(count_t const)depth {
+- (void)print:(size_t const)depth {
 	indent(depth);
 	fprintf(stderr, "(union\n");
-	for(size_t i = 0; i < count; ++i) [filters[i] print:depth+1];
+	for(size_t i = 0; i < count; i++) [filters[i] print:depth+1];
 	indent(depth);
 	fprintf(stderr, ")\n");
 }
-- (size_t)getUserFilter:(str_t *const)data :(size_t const)size :(count_t const)depth {
+- (size_t)getUserFilter:(str_t *const)data :(size_t const)size :(size_t const)depth {
 	size_t len = 0;
-	for(size_t i = 0; i < count; ++i) {
+	for(size_t i = 0; i < count; i++) {
 		if(i) len += wr(data+len, size-len, " or ");
 		len += [filters[i] getUserFilter:data+len :size-len :depth+1];
 	}
