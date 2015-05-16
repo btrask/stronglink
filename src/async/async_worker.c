5f27b2210c9a33d5bc5716d02f537ff56ddcbaef
Ben Trask <bentrask@comcast.net>
2015-05-16 02:16:41 -0400

    Use [1] trick to simplify async_worker.

--- src/async/async_worker.c
+++ src/async/async_worker.c
@@ -6,11 +6,11 @@
 #include "async.h"
 
 struct async_worker_s {
-	uv_thread_t thread;
-	uv_sem_t sem;
+	uv_thread_t thread[1];
+	uv_sem_t sem[1];
 	async_t *work;
 	async_t *main;
-	uv_async_t async;
+	uv_async_t async[1];
 };
 
 static void work(void *const arg) {
@@ -19,15 +19,15 @@ static void work(void *const arg) {
 	worker->main = async_main;
 	async_main = NULL;
 	for(;;) {
-		uv_sem_wait(&worker->sem);
+		uv_sem_wait(worker->sem);
 		if(!worker->work) break;
 		async_switch(worker->work);
-		uv_async_send(&worker->async);
+		uv_async_send(worker->async);
 	}
 }
 static void enter(void *const arg) {
 	async_worker_t *const worker = arg;
-	uv_sem_post(&worker->sem);
+	uv_sem_post(worker->sem);
 }
 static void leave(uv_async_t *const async) {
 	async_worker_t *const worker = async->data;
@@ -41,33 +41,33 @@ async_worker_t *async_worker_create(void) {
 	if(!worker) {
 		return NULL;
 	}
-	if(uv_sem_init(&worker->sem, 0) < 0) {
+	if(uv_sem_init(worker->sem, 0) < 0) {
 		free(worker);
 		return NULL;
 	}
-	worker->async.data = worker;
-	if(uv_async_init(async_loop, &worker->async, leave) < 0) {
-		uv_sem_destroy(&worker->sem);
+	worker->async->data = worker;
+	if(uv_async_init(async_loop, worker->async, leave) < 0) {
+		uv_sem_destroy(worker->sem);
 		free(worker);
 		return NULL;
 	}
-	if(uv_thread_create(&worker->thread, work, worker) < 0) {
-		uv_sem_destroy(&worker->sem);
-		async_close((uv_handle_t *)&worker->async);
+	if(uv_thread_create(worker->thread, work, worker) < 0) {
+		uv_sem_destroy(worker->sem);
+		async_close((uv_handle_t *)worker->async);
 		free(worker);
 		return NULL;
 	}
-	uv_unref((uv_handle_t *)&worker->async);
+	uv_unref((uv_handle_t *)worker->async);
 	return worker;
 }
 void async_worker_free(async_worker_t *const worker) {
 	if(!worker) return;
 	assert(!worker->work);
-	uv_sem_post(&worker->sem);
-	uv_thread_join(&worker->thread);
-	uv_sem_destroy(&worker->sem);
-	uv_ref((uv_handle_t *)&worker->async);
-	async_close((uv_handle_t *)&worker->async);
+	uv_sem_post(worker->sem);
+	uv_thread_join(worker->thread);
+	uv_sem_destroy(worker->sem);
+	uv_ref((uv_handle_t *)worker->async);
+	async_close((uv_handle_t *)worker->async);
 	free(worker);
 }
 
@@ -75,7 +75,7 @@ void async_worker_enter(async_worker_t *const worker) {
 	assert(worker);
 	assert(!worker->work);
 	worker->work = async_active();
-	uv_ref((uv_handle_t *)&worker->async);
+	uv_ref((uv_handle_t *)worker->async);
 	async_call(enter, worker);
 	// Now on worker thread
 }
@@ -84,6 +84,6 @@ void async_worker_leave(async_worker_t *const worker) {
 	assert(async_active() == worker->work);
 	async_switch(worker->main);
 	// Now on original thread
-	uv_unref((uv_handle_t *)&worker->async);
+	uv_unref((uv_handle_t *)worker->async);
 }
 
