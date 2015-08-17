9ca1adc77fa3106d51d93d10a8ca4f3b2d4bb4f8
Ben Trask <bentrask@comcast.net>
2015-08-16 21:10:43 -0400

    Remove async_poll, didn't work as intended.

--- src/async/async.h
+++ src/async/async.h
@@ -62,8 +62,6 @@ int async_read(uv_stream_t *const stream, size_t const size, uv_buf_t *const out
 int async_write(uv_stream_t *const stream, uv_buf_t const bufs[], unsigned const nbufs);
 int async_tcp_connect(uv_tcp_t *const stream, struct sockaddr const *const addr);
 
-int async_poll(uv_stream_t *const stream, int *const events);
-
 // async_fs.c
 uv_file async_fs_open(const char* path, int flags, int mode);
 int async_fs_close(uv_file file);
